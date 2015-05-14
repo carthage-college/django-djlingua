84b8df248c9ed7e04b7ce7939018b126b6881d9f
Bender <plungerman@gmail.com>
2015-05-14 11:07:32 -0500

    new defaults for wsgi and settings

--- djlingua/wsgi_default.py
+++ djlingua/wsgi_default.py
@@ -4,22 +4,22 @@ import os, sys
 sys.path.append('/usr/lib/python2.7/')
 sys.path.append('/usr/lib/python2.7/dist-packages/')
 sys.path.append('/usr/local/lib/python2.7/dist-packages/')
-sys.path.append('/data2/django_current/')
-sys.path.append('/data2/django_projects/')
-sys.path.append('/data2/django_third/')
+sys.path.append('/usr/local/django_current/')
+sys.path.append('/usr/local/django_projects/')
+sys.path.append('/usr/local/django_third/')
 # Django
 os.environ['DJANGO_SETTINGS_MODULE'] = 'djlingua.settings'
 os.environ['PYTHON_EGG_CACHE'] = '/var/cache/python/.python-eggs'
 os.environ['TZ']='America/Chicago'
 # informix
-os.environ['INFORMIXSERVER'] = 'wilson'
-os.environ['DBSERVERNAME'] = 'wilson'
-os.environ['INFORMIXDIR'] = '/opt/ibm/informix'
-os.environ['ODBCINI'] = '/etc/odbc.ini'
-os.environ['ONCONFIG'] = 'onconf.carstrain'
-os.environ['INFORMIXSQLHOSTS'] = '/opt/ibm/informix/etc/sqlhosts'
-os.environ['LD_LIBRARY_PATH'] = '$INFORMIXDIR/lib:$INFORMIXDIR/lib/esql:$INFORMIXDIR/lib/tools:/usr/lib/apache2/modules:$INFORMIXDIR/lib/cli'
-os.environ['LD_RUN_PATH'] = '/opt/ibm/informix/lib:/opt/ibm/informix/lib/esql:/opt/ibm/informix/lib/tools:/usr/lib/apache2/modules'
+os.environ['INFORMIXSERVER'] = ''
+os.environ['DBSERVERNAME'] = ''
+os.environ['INFORMIXDIR'] = ''
+os.environ['ODBCINI'] = ''
+os.environ['ONCONFIG'] = ''
+os.environ['INFORMIXSQLHOSTS'] = ''
+os.environ['LD_LIBRARY_PATH'] = ''
+os.environ['LD_RUN_PATH'] = ''
 # wsgi
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
